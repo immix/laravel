4b520cf416363e329ea209083ecef0d9437fd9dd
Michael Meyer <m.meyer2k@gmail.com>
2014-10-21 17:35:00 -0500

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,3 +1,18 @@
-# this script speeds up vagrant provisioning by only running composer 
-# the first time the vm is booted
+#
+# Provision.sh
+#
+# A vagrant provisioning helper script
+#
+
+# This portion of the script speeds up vagrant provisioning by only running composer 
+# the first time the vm is booted.
 if [ ! -f vendor/autoload.php ]; then composer --prefer-dist install; fi
+
+# Run laravel migrations
+php artisan migrate
+
+# Run laravel database seeder
+php artisan db:seed
+
+# Import any SQL bits provided by the user
+mysql --user=homestead --password=secret < /var/nomad/vagrant/import.sql
