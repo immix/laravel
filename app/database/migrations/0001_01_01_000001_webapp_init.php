def2d7af6aa71f9221c57d9cf879e5981c7dcd48
Michael Meyer <m.meyer2k@gmail.com>
2014-10-22 10:34:28 -0500

    Update 0001_01_01_000001_webapp_init.php

--- app/database/migrations/0001_01_01_000001_webapp_init.php
+++ app/database/migrations/0001_01_01_000001_webapp_init.php
@@ -13,10 +13,18 @@ class WebappInit extends Migration
      */
     public function up()
     {
-        $u = \Config::get('database.connections.mysql.username');
+	// Build your schema here...
+	
+	// Import any data in the import.sql script
+	self::import();
+    }
+    
+    private static function import()
+    {
+    	$u = \Config::get('database.connections.mysql.username');
         $p = \Config::get('database.connections.mysql.password');
         $h = \Config::get('database.connections.mysql.hostname');
-		$d = \Config::get('database.connections.mysql.database');
+	$d = \Config::get('database.connections.mysql.database');
         exec("mysql --host=$h --user=$u --password=$p $d < import.sql");
     }
 
