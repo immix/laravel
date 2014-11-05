f985976b7073a558696a02f8b14fe58a1dfce4f6
Michael Meyer <m.meyer2k@gmail.com>
2014-11-05 14:49:57 -0600

    Update import.sh

--- import.sh
+++ import.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mysqldump --host=localhost --port=3305 --user=vagrant --password=vagrant --database xxxxxxxxx --compact --no-create-info > import.sql
+mysqldump --host=localhost --port=3305 --user=vagrant --password=vagrant --database xxxxxxxxx --compact --no-create-info --single-transaction --skip-lock-tables > import.sql
