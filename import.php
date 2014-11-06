a8ce24666b23e996fdbf007d59a713ab8e0c282c
Michael Meyer <m.meyer2k@gmail.com>
2014-11-06 11:52:36 -0600

    Update import.php

--- import.php
+++ import.php
@@ -7,5 +7,5 @@ $host = '127.0.0.1';
 $pass = '';
 $port = 3305;
 $user = 'root';
-$db = 'immixpro';
+$db = 'database';
 exec("mysqldump --host=$host --port=$port --user=$user --disable-keys --compact --single-transaction --skip-lock-tables --no-create-info --ignore-table=$db.migrations $db > import.sql");
