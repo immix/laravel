9ac2c48da24e54ab04875271d706a05525dbe1fc
Michael Meyer <m.meyer2k@gmail.com>
2014-11-06 11:51:44 -0600

    Delete import.sh

--- import.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mysqldump --host=localhost --port=3305 --user=vagrant --password=vagrant --database xxxxxxxxx --compact --no-create-info --single-transaction --skip-lock-tables > import.sql
