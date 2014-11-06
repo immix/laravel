6a9321af8cfee6af9c3eab244b0d400ce89d7345
Michael Meyer <m.meyer2k@gmail.com>
2014-11-06 11:52:29 -0600

    Update import.php

--- import.php
+++ import.php
@@ -3,9 +3,9 @@
  * Database importer tool for IMMIXCMS applications.
  */
 
-$host = '10.0.1.106';
+$host = '127.0.0.1';
 $pass = '';
 $port = 3305;
 $user = 'root';
 $db = 'immixpro';
-exec("mysqldump --host=127.0.0.1 --port=$port --user=$user --disable-keys --compact --single-transaction --skip-lock-tables --no-create-info --ignore-table=$db.migrations $db > import.sql");
+exec("mysqldump --host=$host --port=$port --user=$user --disable-keys --compact --single-transaction --skip-lock-tables --no-create-info --ignore-table=$db.migrations $db > import.sql");
