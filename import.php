0a0ef0bf397c12315640827f9826e46a4fda1fc7
Michael Meyer <m.meyer2k@gmail.com>
2014-11-06 12:01:45 -0600

    Update import.php

--- import.php
+++ import.php
@@ -7,5 +7,6 @@ $host = '127.0.0.1';
 $pass = '';
 $port = 3305;
 $user = 'root';
-$db = 'database';
-exec("mysqldump --host=$host --port=$port --user=$user --disable-keys --compact --single-transaction --skip-lock-tables --no-create-info --ignore-table=$db.migrations $db > import.sql");
+$db = 'database name';
+exec("echo \"SET FOREIGN_KEY_CHECKS = 0;\" > import.sql");
+exec("mysqldump --host=$host --port=$port --user=$user --disable-keys --compact --single-transaction --skip-lock-tables --no-create-info --ignore-table=$db.migrations $db >> import.sql");
