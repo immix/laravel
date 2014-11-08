40f00d775a6f8189336d05c01b5fbbd8373a5242
Michael Meyer <m.meyer2k@gmail.com>
2014-11-07 18:08:00 -0600

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-if [ ! -f vendor/autoload.php ]; then composer --prefer-dist install; fi
 if [ -f /vagrant/auth.json ]; then cp -f /vagrant/auth.json .; fi
+if [ ! -f vendor/autoload.php ]; then composer --prefer-dist install; fi
 php artisan migrate
