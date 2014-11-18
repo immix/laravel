0b3bad24f6f9d0863132a44ad27c698c7ff29b04
Michael Meyer <m.meyer2k@gmail.com>
2014-11-18 12:43:07 -0600

    Update master.blade.php

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -20,7 +20,7 @@
 
         @show
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 
         @if (App::environment('production'))
