889b1a68ca196c657b013d44ddf6a5f921cdc2d1
Michael Meyer <m.meyer2k@gmail.com>
2014-11-18 12:40:05 -0600

    Update master.blade.php

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,30 +1,45 @@
 <!doctype html>
-<html>
+<html class="no-js" lang="">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+
+    </head>
+
     <body>
-    
+
         @section('content')
-        
+
         <!-- blank content -->
-        
+
         @show
-        
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
         @if (App::environment('production'))
         <script>
             (function (i, s, o, g, r, a, m) {
-            i['GoogleAnalyticsObject'] = r;
-                    i[r] = i[r] || function () {
-            (i[r].q = i[r].q || []).push(arguments)
-            }, i[r].l = 1 * new Date();
-                    a = s.createElement(o),
-                    m = s.getElementsByTagName(o)[0];
-                    a.async = 1;
-                    a.src = g;
-                    m.parentNode.insertBefore(a, m)
-                    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                        m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
             ga('create', "{{ \Config::get('services.google-analytics.key') }}", 'auto');
             ga('send', 'pageview');
         </script>
         @endif
-    
+
     </body>
 </html>
