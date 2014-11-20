763a0dcecefaec2f32c48e505b4c8a1015293ab1
Michael Meyer <m.meyer2k@gmail.com>
2014-11-20 14:59:36 -0600

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -2,3 +2,4 @@
 if [ -f /vagrant/auth.json ]; then cp -f /vagrant/auth.json .; fi
 if [ ! -f vendor/autoload.php ]; then composer --prefer-dist install; fi
 php artisan migrate
+php artisan immix:adduser support@immixproductions.com !password!
